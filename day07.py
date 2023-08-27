a115eac41d39f970ef6d8ba43796f7868f91330a
Rob Mason <robm@librem.one>
2023-08-27 03:27:19 -0400

    7 :)

--- day07.py
+++ day07.py
@@ -1,4 +1,46 @@
-with open('day07.txt') as datafile:
-    cmds = datafile.readlines()
+# with open('day07.txt') as datafile:
+#     cmds = datafile.readlines()
     
-print(cmds)
+# print(cmds)
+
+import sys
+from collections import defaultdict
+infile = sys.argv[1] if len(sys.argv)>1 else 'day07.txt'
+data = open(infile).read().strip()
+lines = [x for x in data.split('\n')]
+
+# directory path -> total size of that dir (including subdirs)
+SZ = defaultdict(int)
+path = []
+for line in lines:
+    words = line.strip().split()
+    if words[1] == 'cd':
+        if words[2] == '..':
+            path.pop()
+        else:
+            path.append(words[2])
+    elif words[1] == 'ls':
+        continue
+    elif words[0] == 'dir':
+        continue
+    else:
+        sz = int(words[0])
+        # add this file's size to the current dir size *and* the size of all parents
+        for i in range(1, len(path)+1):
+            SZ['/'.join(path[:i])] += sz
+
+max_used = 70000000 - 30000000
+total_used = SZ['/']
+need_to_free = total_used - max_used
+
+p1 = 0 
+p2 = 1e9
+for k,v in SZ.items():
+    #print(k,v)
+    if v <= 100000:
+        p1 += v
+    if v>=need_to_free:
+        p2 = min(p2, v)
+
+print(p1)
+print(p2)
